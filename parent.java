953541b1583d0cd70081a0c1eaa741d9d66e9329
Pranta Kumer Pandit <148738032+pranta2003@users.noreply.github.com>
2025-09-26 01:50:29 +0600

    Create parent.java

---
+++ parent.java
@@ -0,0 +1,12 @@
+//method hiding
+class Parent {
+    public static void main(String[] args) {
+        System.out.println("Parent main()");
+    }
+}
+
+class Child extends Parent {
+    public static void main(String[] args) {
+        System.out.println("Child main()");
+    }
+}
