8055595c93c926bc128dd5f03cd5de38cd51d870
Pranta Kumer Pandit <148738032+pranta2003@users.noreply.github.com>
2025-09-26 01:44:46 +0600

    Create two_dimention_array.java

---
+++ two_dimention_array.java
@@ -0,0 +1,18 @@
+public class two_dimention_array {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Print matrix elements
+        System.out.println("Matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
