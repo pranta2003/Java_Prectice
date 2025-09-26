20f0955f008573544c1ca0db40d26bdd285aaa78
Pranta Kumer Pandit <148738032+pranta2003@users.noreply.github.com>
2025-09-26 13:14:03 +0600

    Create MultilevelInheritance_dog_cat.java

---
+++ MultilevelInheritance_dog_cat.java
@@ -0,0 +1,21 @@
+class Animal
+{  
+void eat(){System.out.println("eating !!!");}  
+}  
+class Dog extends Animal
+{  
+void bark(){System.out.println("barking !!!");}  
+}  
+class BabyDog extends Dog
+{  
+void weep(){System.out.println("weeping !!!");}  
+}  
+class MultilevelInheritance_dog_cat
+{   public static void main(String args[])
+  {  
+   BabyDog d=new BabyDog();  
+   d.weep();  
+   d.bark();  
+   d.eat();  
+  }
+} 
