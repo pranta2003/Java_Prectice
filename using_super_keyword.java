d7fa0ffbc8a4a9c63a44c8db2217a5f05d40d9d2
Pranta Kumer Pandit <148738032+pranta2003@users.noreply.github.com>
2025-09-26 14:16:22 +0600

    Create using_super_keyword.java

---
+++ using_super_keyword.java
@@ -0,0 +1,23 @@
+class Parent {
+    Parent(String name) {
+        System.out.println("Parent constructor: " + name);
+    }
+}
+
+class Child extends Parent {
+    Child() {
+String s;
+        super("Maharin"); // calls Parent constructor
+        System.out.println("Child constructor");
+    }
+}
+
+public class using_super_keyword {
+    public static void main(String[] args) {
+ Child c= new Child();
+    }
+}
+
+Output:
+Parent constructor: Maharin
+Child constructor
